74b1fd32ed7f55fac813f58c94317095b057a19c
Jupiter0909 <61379653+Jupiter0909@users.noreply.github.com>
2020-12-18 13:12:21 +0800

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div className="max-w-sm rounded overflow-hidden shadow-lg">
+      <img src={require('./landing-page-girl.png')} alt="Display" className="w-full" />
+      <div className="px-6 py-4">
+        <div className="font-bold text-purple-500 text-xl mb-2">
+          Blessing Krofegha
+        </div>
+        <p className="text-gray-700 text-base">
+          When im not coding i switch to netflix with cold coffee
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
+      <div className="px-6 py-4">
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
+          #Software Engineer
+        </span>
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
+          #Writer
+        </span>
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
+          #Public Speaker
+        </span>
+      </div>
     </div>
   );
 }
